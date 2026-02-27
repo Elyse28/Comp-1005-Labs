afac7300654590327400b7697b1daa48c05fafd8
Elyse <e.zakreski24@gmail.com>
2026-02-26 19:14:56 -0500

    i did finished assingment 2

--- Assignmet 2/a2q1.py
+++ Assignmet 2/a2q1.py
@@ -1,11 +1,11 @@
 def seeds():
-    name = str(input("Enter your name: "))
-    print("Please select a bundle from the options below: \n\t1: [Beginner] A beginners bundle (5.00$) \n\t2: [Chef] Chefs Bundle (10.00$) \n\t3: [Hobbiest] A hobbiest bundle (15.00$)")
-    bundles = {1: ("Beginner", 5), 2: ("Chef", 10), 3: ("Hobbiest", 15)}
+    name = input("Enter your name: ")
+    print("Please select a bundle from the options below: \n\t1: [Florist] A Florist bundle (5.00$) \n\t2: [Chef] Chefs Bundle (10.00$) \n\t3: [Gardener] A gardener bundle (15.00$)")
+    bundles = {1: ("Florist", 5), 2: ("Chef", 10), 3: ("Gardener", 15)}
     choice = int(input(""))
     while choice != 1 and choice != 2 and choice != 3:
         print("Error: Invalid bundle number, please enter 1, 2, or 3")
-        print("Please select a bundle from the options below: \n\t1: [Beginner] A beginners bundle (5.00$) \n\t2: [Chef] Chefs Bundle (10.00$) \n\t3: [Hobbiest] A hobbiest bundle (15.00$)")
+        print("Please select a bundle from the options below: \n\t1: [Florist] A Florist bundle (5.00$) \n\t2: [Chef] Chefs Bundle (10.00$) \n\t3: [Gardener] A gardener bundle (15.00$)")
         choice = int(input(""))
     num = int(input("Enter the quantity of bundles you want: "))
     total = num * bundles[choice][1]
